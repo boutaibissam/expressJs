001f4a7dbc3cbd02473873b5f47a5feab0edc16d
boutaibissam <boutaibism95@gmail.com>
2017-09-10 16:48:00 +0100

    first commit

---
+++ routes/index.js
@@ -0,0 +1,21 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/home', function(req, res, next) {
+  res.render('index', { title: 'Express' });
+});
+
+// Mark: about page 
+router.get('/about',function(req, res, next){
+  res.render('about',{
+    title: 'About'
+  });
+});
+
+// Mark: Contact
+router.get('/contact', function(req, res, next){
+  res.render('contact', {title: 'Contact'});
+});
+
+module.exports = router;
